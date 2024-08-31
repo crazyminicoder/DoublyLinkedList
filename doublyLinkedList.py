32693a23caf40dab67ec3474f8c981d0124579da
Syed Wali Uddin Quadri <waliwasser29@gmail.com>
2024-08-31 10:50:01 -0500

    Added peek Head and Tail methods

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -8,6 +8,7 @@ class Node:
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def insert(self, data):
         new_node = Node(data)
@@ -19,6 +20,7 @@ class DoublyLinkedList:
             last = last.next
         last.next = new_node
         new_node.prev = last
+        self.tail = new_node
 
     def printBackward(self):
         current = self.head
@@ -27,6 +29,20 @@ class DoublyLinkedList:
         while current:
             print(current.data, end=" <-> " if current.prev else "\n")
             current = current.prev
+
+    def peekHead(self):
+        if self.head is None:
+            print('The list is empty')
+            return
+        temp = self.head
+        print('Head is', temp.data)
+
+    def peekTail(self):
+        if self.tail is None:
+            print('The list is empty')
+            return
+        temp = self.tail
+        print('Tail is:', temp.data)
 
 
 dll = DoublyLinkedList()
@@ -38,3 +54,6 @@ dll.insert(25)
 dll.insert(30)
 
 dll.printBackward()
+
+dll.peekHead()
+dll.peekTail()
